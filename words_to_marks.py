da2f34d42b65d25f9478172adb929b269d951da3
semydava <valeriia.a.semida@gmail.com>
2020-09-16 15:34:37 +0200

    edit

--- words_to_marks.py
+++ words_to_marks.py
@@ -3,7 +3,6 @@ def words_to_marks(s):
   sum_char = 0
   characters =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   for char in s:
-    if char in characters:
-      number = characters.index(char) + 1
-      sum_char+=number
+  	number = characters.index(char) + 1
+  	sum_char+=number
   return sum_char
