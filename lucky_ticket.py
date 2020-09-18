f4173209c861a65df6a3f5c98887dd140319937b
semydava <valeriia.a.semida@gmail.com>
2020-09-18 15:24:58 +0200

    change array to dictionary

--- lucky_ticket.py
+++ lucky_ticket.py
@@ -3,19 +3,19 @@ def is_lucky(ticket):
     first_digits = ticket [:3]
     last_digits = ticket[3:]
 
-    i = 0
+    
     sum_first_digits = 0
     sum_last_digits = 0
 
     if len(ticket) != 6:
       return False
 
-    while i < len(first_digits) and i < len(last_digits):
-      if not first_digits[i].isdigit() or not last_digits[i].isdigit():
+    for (char1,char2) in zip(first_digits,last_digits):
+      if not char1.isdigit() or not char2.isdigit():
         return False
 
-      sum_first_digits += int(first_digits[i])
-      sum_last_digits += int(last_digits[i])
-      i +=1
+      sum_first_digits += int(char1)
+      sum_last_digits += int(char2)
+      
 
     return sum_last_digits == sum_first_digits
