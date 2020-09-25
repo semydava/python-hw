0d3ce3846c82c1571750b697dd94bedeadd5f2c3
semydava <valeriia.a.semida@gmail.com>
2020-09-25 15:37:33 +0200

    add condition whether number_repeat empty

--- most_frequent_word.py
+++ most_frequent_word.py
@@ -12,7 +12,10 @@ def most_frequent_word(text):
       number = text.count(char) 
       if number > 1: 
         number_repeat[char] = number
-    maximum = max(number_repeat.values())
-    for key, value in number_repeat.items():
-      if value == maximum:
-        return key 
+    if number_repeat: 
+      maximum = max(number_repeat.values())
+      for key, value in number_repeat.items():
+        if value == maximum:
+          return key 
+    else: 
+      return 'All words are equal'
