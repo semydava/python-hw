72272691c3f12b2c00f9065d1aaced8d7ad81208
semydava <valeriia.a.semida@gmail.com>
2020-10-01 16:45:27 +0200

    errors correction

--- is_balanced.py
+++ is_balanced.py
@@ -7,6 +7,10 @@ def is_balanced(text):
   True
   >>> is_balanced('))((')
   False
+  >>> is_balanced('(]')
+  False
+  >>> is_balanced('[()[]]')
+  True
   >>> is_balanced('(Sensei says yes!)')
   True
   >>> is_balanced('(Sensei says no!')
@@ -33,15 +37,15 @@ def is_balanced(text):
   
 
   for element in brackets:
-    if element in possible_brackets.keys():
+    if element in possible_brackets:
       brackets_stack.append(element)
+     
     else:
       if len(brackets_stack)==0:
         return False
       current_char = brackets_stack.pop()
       
-      if current_char == possible_brackets.keys():
-        if element != possible_brackets.values():
+      if element != possible_brackets[current_char]:
           return False
         
 
@@ -49,4 +53,4 @@ def is_balanced(text):
 
   
   
-print(is_balanced('(Sensei (says) (yes!))'))
+print(is_balanced('(Sensei says yes!)'))
