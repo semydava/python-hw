6b21d25630bfe7deae810081d9537ca8b36ed531
semydava <valeriia.a.semida@gmail.com>
2020-10-27 15:13:08 +0100

    write the result in new list + split code to several functions

--- mergesort_algorithm.py
+++ mergesort_algorithm.py
@@ -1,31 +1,35 @@
-def mergesort(text):
-  if len(text) > 1:
-    middle = len(text) // 2
-    left_part = text[:middle]
-    right_part = text[middle:]
-    i = 0
-    j = 0
-    k = 0
-    mergesort(left_part)
-    mergesort(right_part)
-    while i < len(left_part) and j < len(right_part):
-      if left_part[i] < right_part[j]:
-        text[k] = left_part[i]
-        i+=1
-      else:
-        text[k] = right_part[j]
-        j+=1
-      k+=1
-    while i < len(left_part):
-      text[k] = left_part[i]
+def split(input_data):
+  middle = len(input_data) // 2
+  left_part = input_data[:middle]
+  right_part = input_data[middle:]
+  return left_part, right_part
+
+def merge_list(left_part,right_part):
+  sorted_text = []
+  i = 0
+  j = 0
+  if len(left_part) == 0:
+    return right_part
+  if len(right_part) == 0:
+    return left_part
+  while len(sorted_text) < (len(left_part) + len(right_part)):
+    if left_part[i] < right_part[j]:
+      sorted_text.append(left_part[i])
       i+=1
-      k+=1  
-    while j < len(right_part):
-      text[k] = right_part[j]
+    else:
+      sorted_text.append(right_part[j])
       j+=1
-      k+=1
-  return text
-      
+    if i == len(left_part):
+      sorted_text+=right_part[j:]
+      break
+    elif j == len(right_part):
+      sorted_text+=left_part[i:]
+      break
+  return sorted_text
 
-
-print(mergesort([54]))
+def mergesort(input_data):
+  if len(input_data) <= 1:
+    return input_data
+  else:
+    left_part,right_part = split(input_data)
+    return merge_list(mergesort(left_part),mergesort(right_part))
