f279f5252dc020663478aef90016a4fbc7b3beb1
semydava <valeriia.a.semida@gmail.com>
2021-01-30 12:53:44 +0100

    new version

--- Football_game/football_game.py
+++ Football_game/football_game.py
@@ -55,7 +55,7 @@ def wins(games):
 
 def most_wins(games, names):
     team_by_wins = wins(games)
-    print(team_by_wins)
+    # print(team_by_wins)
     team_id_most_wins = max(team_by_wins, key=team_by_wins.get)
     team_name_most_wins = id_to_name(names)[team_id_most_wins]
     return team_name_most_wins
@@ -97,7 +97,7 @@ def ten_winners(games, names):
     sorted_team2016 = sorted(team_by_wins_2016.items(), key=lambda x: x[1], reverse=True)
     best_10id_2016 = [x[0] for x in sorted_team2016[0:10]]
     best_10names_2016 = []
-    print(best_10id_2016)
+    # print(best_10id_2016)
     all_names = id_to_name(names)
     for id in best_10id_2016:
         name = all_names[id]
@@ -107,57 +107,148 @@ def ten_winners(games, names):
 
 #print(ten_winners(open('football_teams.csv'), open('teams.csv')))
 
-#Пропущенный вс забитый гол
-def goals(games):
-    scored_goals_by_id = {}
-    missed_goals_by_id = {}
-    for row in csv.DictReader(games):
-        scored_goals_by_id[int(row['team_a'])] = int(row['score'].split(':')[0])
-        scored_goals_by_id[int(row['team_b'])] = int(row['score'].split(':')[1])
-        missed_goals_by_id[int(row['team_a'])] = int(row['score'].split(':')[1])
-        missed_goals_by_id[int(row['team_b'])] = int(row['score'].split(':')[0])
-    return scored_goals_by_id, missed_goals_by_id
-print(goals(open('football_teams.csv')))
 
 
-
-def points(games):
-    points_by_id = {}
-    for row in csv.DictReader(games):
-        if int(row['score'].split(':')[0]) < int(row['score'].split(':')[1]):
-            key = int(row['team_b'])
-            if key in points_by_id.keys():
-                points_by_id[key] += 3
-            else:
-                points_by_id[key] = 3
-
-        elif int(row['score'].split(':')[0]) > int(row['score'].split(':')[1]):
-            key = int(row['team_a'])
-            if key in points_by_id:
-                points_by_id[key] += 3
-            else:
-                points_by_id[key] = 3
-        else:
-            if int(row['team_a']) in points_by_id.keys() and int(row['team_b']) in points_by_id.keys():
-                points_by_id[int(row['team_a'])] +=1
-                points_by_id[int(row['team_b'])] +=1
-            elif int(row['team_a']) in points_by_id.keys() and int(row['team_b']) not in points_by_id.keys():
-                points_by_id[int(row['team_a'])] += 1
-                points_by_id[int(row['team_b'])] = 1
-            elif int(row['team_a']) not in points_by_id.keys() and int(row['team_b']) in points_by_id.keys():
-                points_by_id[int(row['team_a'])] = 1
-                points_by_id[int(row['team_b'])] += 1
-            else:
-                points_by_id[int(row['team_a'])] = 1
-                points_by_id[int(row['team_b'])] = 1
-
-    return points_by_id
-print(points(open('football_teams.csv')))
 
 # Отобразить турнирную таблицу[1] всех команд игравших в 2016 году
-def tournament_table(games,names):
-    name_by_id = id_to_name(names)
+def get_table():
+    teams_raw_data = get_teams_raw_data(open('teams.csv'))
+    out = {}
 
-    print(tabulate(name_by_id.items(), headers=['ID', 'Name'], tablefmt="grid"))
+    for id, values in teams_raw_data.items():
+        points = values['points']
+        position = int(get_position(points, id))
+        if position in out.keys():
+            i = 1
+            while position + i in out.keys():
+                i += 1
+            position = position + i
+            if position not in out.keys():
+                out[position] = int(id)
+                # print(out)
+        else:
+            out[position] = int(id)
+    # print(out)
+    out = sorted(out.items())
+    return out
+
+#get team position in the final table
+def get_position(points, id):
+    teams_raw_data = get_teams_raw_data(open('teams.csv'))
+    position = 1
+    for current_id, values in teams_raw_data.items():
+        if values['points'] > points:
+            position += 1
+        elif values['points'] == points:
+            if id in values.keys():
+                wins_current = values['wins_record'][id]
+                wins_requested = get_wins_by_competitor(id, current_id)
+                if wins_requested < wins_current:
+                    position += 1
+                elif wins_current == wins_requested:
+                    current_general_wins = values['wins']
+                    requested_general_wins = get_field_by_team_id(id, 'wins')
+                    if requested_general_wins < current_general_wins:
+                        position += 1
+                    elif current_general_wins == requested_general_wins:
+                        current_goal_diff = values['wins']
+                        requested_goal_diff = get_field_by_team_id(id, 'goals_difference')
+                        if requested_goal_diff < current_goal_diff:
+                            position += 1
+                        elif current_goal_diff == requested_goal_diff:
+                            current_scored_goals = values['scored_goals']
+                            requested_scored_goals = get_field_by_team_id(id, 'scored_goals')
+                            if requested_scored_goals < current_scored_goals:
+                                position += 1
+    # print(position)
+    return position
+
+#get wins in match with particular competitor
+def get_wins_by_competitor(team_id, competitor_id):
+    teams_raw_data = get_teams_raw_data(open('teams.csv'))
+    wins = 0
+    for id, values in teams_raw_data.items():
+        if id == team_id and competitor_id in values['wins_record'].keys():
+            wins = values['wins_record'][competitor_id]
+    return wins
+
+#get the value from stats by field
+def get_field_by_team_id(team_id, field):
+    teams_raw_data = get_teams_raw_data(open('teams.csv'))
+    value = 0
+    for id, values in teams_raw_data.items():
+        if id == team_id and field in values.keys():
+            value = values[field]
+    return value
+
+#get raw data with name
+def get_teams_raw_data(names):
+    raw_table = {}
+    for row in csv.DictReader(names):
+        raw_table[row['id']] = get_team_stats(row['id'])
+        raw_table[row['id']]['name'] = row['name']
+    return raw_table
+
+#collect team statistics
+def get_team_stats(id):
+    stats = {}
+    win = 0
+    draws = 0
+    losses = 0
+    scored_goals = 0
+    missed_goals = 0
+    for row in csv.DictReader(open('football_teams.csv')):
+        if '2016' not in row['date']:
+            print('load' + id + row['team_b'])
+            if id == row['team_b'] or id == row['team_a']:
+
+                if 'wins_record' not in stats.keys():
+                    stats['wins_record'] = {}
+
+                if id == row['team_b']:
+                    scored_goals += int(row['score'].split(':')[1])
+                    missed_goals += int(row['score'].split(':')[0])
+                else:
+                    scored_goals += int(row['score'].split(':')[0])
+                    missed_goals += int(row['score'].split(':')[1])
+
+                if (id == row['team_a'] and int(row['score'].split(':')[0]) > int(row['score'].split(':')[1])) or (
+                        id == row['team_b'] and int(row['score'].split(':')[0]) < int(row['score'].split(':')[1])):
+                    win += 1
+                    if id == row['team_b']:
+                        if row['team_a'] in stats.keys():
+                            stats['wins_record'][row['team_a']] += 1
+                        else:
+                            stats['wins_record'][row['team_a']] = 1
+                    else:
+                        if row['team_b'] in stats.keys():
+                            stats['wins_record'][row['team_b']] += 1
+                        else:
+                            stats['wins_record'][row['team_b']] = 1
+
+                elif int(row['score'].split(':')[0]) == int(row['score'].split(':')[1]):
+                    draws += 1
+                else:
+                    losses += 1
+    stats['wins'] = win
+    stats['losses'] = losses
+    stats['draws'] = draws
+    stats['scored_goals'] = scored_goals
+    stats['goals_difference'] = scored_goals - missed_goals
+    stats['points'] = int(stats['wins']) * 3 + int(stats['draws'])
+
+    return stats
+
+
+
+# def prn():
+    # return get_team_stats(87)
+print(get_table())
+    # print(get_team_wins(open('football_teams.csv')))
+#def tournament_table(games,names):
+# print(get_team_stats('87'))
+    #name_by_id = id_to_name(names)
+
+    #print(tabulate(name_by_id.items(), headers=['ID', 'Name'], tablefmt="grid"))
 
 #print(tournament_table(open('football_teams.csv'),open('teams.csv')))
